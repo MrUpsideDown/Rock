90840dd0fb912844b6c61f0330fd08440727878e
MrUpsideDown <mr.upside.down@gmail.com>
2015-06-13 09:53:18 +1000

    Modified Group Detail block to only display Attributes with "Show in Grid" flag enabled. This makes the Group Viewer easier to use with Groups that have many custom Attributes.

--- RockWeb/Blocks/Groups/GroupDetail.ascx.cs
+++ RockWeb/Blocks/Groups/GroupDetail.ascx.cs
@@ -1169,9 +1169,12 @@ namespace RockWeb.Blocks.Groups
             group.LoadAttributes();
             attributes.AddRange( group.Attributes.Select( a => a.Value ).OrderBy( a => a.Order ).ThenBy( a => a.Name ) );
 
-            // display attribute values
+            // Display Attribute Values that have the "Display as Grid Column" flag enabled.
             var attributeCategories = Helper.GetAttributeCategories( attributes );
-            Rock.Attribute.Helper.AddDisplayControls( group, attributeCategories, phAttributes, null, false );
+
+            var excludedAttributes = attributes.Where(x => !x.IsGridColumn).Select(x => x.Name).ToList();
+
+            Rock.Attribute.Helper.AddDisplayControls( group, attributeCategories, phAttributes, excludedAttributes, false );
 
             var pageParams = new Dictionary<string, string>();
             pageParams.Add("GroupId", group.Id.ToString());
